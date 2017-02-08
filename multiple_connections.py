758cc742dd6834c71daa55d61f8c71a2f51d7399
bjmb <jaume.marhuenda@datastax.com>
2017-02-08 15:09:58 -0500

    Another poc

--- multiple_connections.py
+++ multiple_connections.py
@@ -1,66 +1,21 @@
-
-import logging
-import traceback
-from functools import partial
-
-from cassandra.cluster import Cluster, NoHostAvailable
-from tests.integration.long.utils import wait_for_down, wait_for_up, force_stop, start
 import time
 from ccmlib.cluster import Cluster as CCMCluster
-
-
-from cassandra.io.libevreactor import LibevConnection
 from cassandra.io.asyncorereactor import AsyncoreConnection
-
-from tests.integration import execute_with_long_wait_retry, drop_keyspace_shutdown_cluster, MockLoggingHandler
-from tests.integration.standard.utils import create_table_with_all_types
-import sys
-from cassandra.policies import HostDistance
-from cassandra.query import tuple_factory, SimpleStatement
-from itertools import cycle
-from cassandra import InvalidRequest, ConsistencyLevel, ReadTimeout, WriteTimeout, OperationTimedOut, \
-    ReadFailure, WriteFailure
-from cassandra.concurrent import execute_concurrent, execute_concurrent_with_args, ExecutionResult
-from threading import Event
+from cassandra import  ConsistencyLevel
 from cassandra.protocol import QueryMessage
+import logging
 
 connection_class = AsyncoreConnection
 
 
-keyspace_name = "testspace"
-
-
-log = logging.getLogger()
-log.setLevel('WARNING')
-
 PROTOCOL_VERSION = 4
 
 
-import logging
 
 log = logging.getLogger()
-log.setLevel('INFO')
-
-
-def get_connection(timeout=5):
-    """
-    Helper method to solve automated testing issues within Jenkins.
-    Officially patched under the 2.0 branch through
-    17998ef72a2fe2e67d27dd602b6ced33a58ad8ef, but left as is for the
-    1.0 branch due to possible regressions for fixing an
-    automated testing edge-case.
-    """
-    e = None
-
-    conn = connection_class.factory(host='127.0.0.1', timeout=timeout, protocol_version=PROTOCOL_VERSION)
-
-    if conn:
-        return conn
-    else:
-        raise e
+log.setLevel('DEBUG')
 
 def make_request():
-    N = 1
     log.debug("\n\nMake request called\n\n")
 
     query = "SELECT keyspace_name FROM system.schema_keyspaces LIMIT 1"
@@ -69,20 +24,20 @@ def make_request():
         pass
 
     conn = connection_class.factory(host='127.0.0.1', timeout=5, protocol_version=PROTOCOL_VERSION)
+    log.debug("Got connection, everything should be good for query")
     conn.send_msg( QueryMessage(query=query, consistency_level=ConsistencyLevel.ONE), request_id=0, cb=cb)
     conn.close()
 
     log.debug("\n\nRequest ended\n\n")
 
 
-CCM_KWARGS = {}
-CCM_KWARGS['version'] = "3.7"
-
-path_to_ccm = "/home/jaume/.ccm/"
-
 setup_cluster = False
 
 if setup_cluster:
+    CCM_KWARGS = {}
+    CCM_KWARGS['version'] = "3.7"
+
+    path_to_ccm = "/home/jaume/.ccm/"
 
     CCM_CLUSTER = CCMCluster(path_to_ccm, "test_for_is_conn", **CCM_KWARGS)
     CCM_CLUSTER.populate(3)
@@ -92,33 +47,9 @@ if setup_cluster:
 
 connection_class.initialize_reactor()
 
-log.setLevel('DEBUG')
-
-mock_handler = MockLoggingHandler()
-logger = logging.getLogger()
-logger.addHandler(mock_handler)
-
-
 try:
-    for _ in range(4000):
-        #connection_class.initialize_reactor()
-        for _ in range(10):
-            log.setLevel("WARNING")
-            try:
-                #get_connection(5)
-                conn = get_connection(sys.float_info.min)
-                #log.info("Went through the tiny timeout!")
-                #make_request_with_conn(conn)
-                conn.close()
-            except Exception as e:
-                log.info(e)
-                log.info("Timeout generated")
-                pass
-            log.debug("\n\n\n\n\n")
-            log.setLevel("DEBUG")
+    for _ in range(40000):
             make_request()
-            #mock_handler.reset()
-        #connection_class.initialize_reactor()
 
 except Exception as e:
     import traceback;traceback.print_exc()
